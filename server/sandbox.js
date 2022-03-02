c36bbb529300add7e608d94332439ca6d12a0232
Raph Vint <electromaniak60@gmail.com>
2022-03-02 16:54:25 +0100

    feat(shop): scrape new products

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,21 +1,40 @@
 /* eslint-disable no-console, no-process-exit */
-const dedicatedbrand = require('./sources/dedicatedbrand');
+const dedicatedbrand = require('./dedicatedbrand');
+const adresse = require('./adresse');
+const fs = require('fs');
 
-async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
+async function sandbox() {
   try {
-    console.log(`🕵️‍♀️  browsing ${eshop} source`);
+    let allProducts = [];
+    const pages = await dedicatedbrand.scrapeLinks();
+    for (const page of pages) {
+      console.log(`🕵️‍♀️  browsing ${page} source`);
+      const products = await dedicatedbrand.scrape(page);
+      // console.log(products);
+      console.log('done');
+      allProducts = allProducts.concat(products);
+    }
 
-    const products = await dedicatedbrand.scrape(eshop);
+    allProducts = Array.from(new Set(allProducts.map(p => p.name)))
+      .map(name => {
+        return allProducts.find(p => p.name === name);
+      });
 
-    console.log(products);
-    console.log('done');
-    process.exit(0);
+    let products = await adresse.scrape();
+    allProducts = allProducts.concat(products);
+    const json = JSON.stringify(allProducts, null, 2);
+    fs.writeFile('products.json', json, 'utf8', () => {
+      console.log(allProducts.length);
+      process.exit(0);
+    });
   } catch (e) {
     console.error(e);
     process.exit(1);
   }
 }
 
-const [,, eshop] = process.argv;
+// const [, , eshop] = process.argv;
 
-sandbox(eshop);
+fs.truncate('./products.json', 0, err => {
+  sandbox();
+});
