4d2b50fcd3b4a3f0c59b1fa77275fd7cf5bd6e51
Raph Vint <electromaniak60@gmail.com>
2022-03-20 19:51:02 +0100

    connect to mongo db

--- server/db/index.js
+++ server/db/index.js
@@ -4,14 +4,13 @@ const fs = require('fs');
 
 const MONGODB_DB_NAME = 'clearfashion';
 const MONGODB_COLLECTION = 'products';
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://alozor:ocrmBRSaJQlfGtxL@clear-fashion-cluster.xriol.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 let client = null;
 let database = null;
 
 /**
  * Get db connection
- * @type {MongoClient}
  */
 const getDB = module.exports.getDB = async () => {
   try {
@@ -20,7 +19,7 @@ const getDB = module.exports.getDB = async () => {
       return database;
     }
 
-    client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
+    client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true, useUnifiedTopology: true});
     database = client.db(MONGODB_DB_NAME);
 
     console.log('💽  Connected');
@@ -55,16 +54,61 @@ module.exports.insert = async products => {
   }
 };
 
-/**
- * Find products based on query
- * @param  {Array}  query
- * @return {Array}
- */
+module.exports.findBrand = async brand => {
+  const db = await getDB();
+  const collection = db.collection(MONGODB_COLLECTION);
+  const products = await collection.find({brand: brand}).toArray();
+
+  return products;
+};
+
+module.exports.findPrice = async price => {
+  const db = await getDB();
+  const collection = db.collection(MONGODB_COLLECTION);
+  const products = await collection.find({price: {$lt: price}}).toArray();
+
+  return products;
+};
+
+module.exports.findSorted = async () => {
+  const db = await getDB();
+  const collection = db.collection(MONGODB_COLLECTION);
+  const products = await collection.find().sort({'price': 1}).toArray();
+
+  return products;
+};
+
 module.exports.find = async query => {
   try {
     const db = await getDB();
     const collection = db.collection(MONGODB_COLLECTION);
     const result = await collection.find(query).toArray();
+
+    return result;
+  } catch (error) {
+    console.error('🚨 collection.find...', error);
+    return null;
+  }
+};
+
+module.exports.findAndSort = async (query, sort) => {
+  try {
+    const db = await getDB();
+    const collection = db.collection(MONGODB_COLLECTION);
+    const result = await collection.find(query).sort(sort).toArray();
+
+    return result;
+  } catch (error) {
+    console.error('🚨 collection.find...', error);
+    return null;
+  }
+};
+
+module.exports.findSortAndLimit = async (query, sort, limit) => {
+  try {
+    const db = await getDB();
+    const collection = db.collection(MONGODB_COLLECTION);
+    const result = await collection.find(query).sort(sort).limit(limit).toArray();
 
     return result;
   } catch (error) {
